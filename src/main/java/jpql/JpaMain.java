4a3e2011f850773ea8a066cc838e97e44f4bc239
ChoiMunSeok <138023245+Mun-Seok@users.noreply.github.com>
2024-01-03 19:43:13 +0900

    Paging

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -50,13 +50,13 @@ public class JpaMain {
 //            System.out.println("result = " + result.getUsername());
 
 
-            Member member = new Member();
-            member.setUsername("member1");
-            member.setAge(10);
-            em.persist(member);
-
-            em.flush();
-            em.clear();
+//            Member member = new Member();
+//            member.setUsername("member1");
+//            member.setAge(10);
+//            em.persist(member);
+//
+//            em.flush();
+//            em.clear();
 
 //            List<Member> result = em.createQuery("select m from Member m", Member.class) // 엔티티 프로젝션 -> 엔티티의 데이터 모두 영속성 컨텍스트에 관리됨
 //                    .getResultList();
@@ -92,12 +92,35 @@ public class JpaMain {
 //            System.out.println("age = " + result[1]);
 
             // 3. new 명령어로 조회
-            List<MemberDTO> result = em.createQuery("select new jpql.MemberDTO(m.username, m.age) from Member m", MemberDTO.class) // 생성자 통해 호출됨
-                    .getResultList();
+//            List<MemberDTO> result = em.createQuery("select new jpql.MemberDTO(m.username, m.age) from Member m", MemberDTO.class) // 생성자 통해 호출됨
+//                    .getResultList();
+//
+//            MemberDTO memberDTO = result.get(0);
+//            System.out.println("memberDTO = " + memberDTO.getUsername());
+//            System.out.println("memberDTO = " + memberDTO.getAge());
 
-            MemberDTO memberDTO = result.get(0);
-            System.out.println("memberDTO = " + memberDTO.getUsername());
-            System.out.println("memberDTO = " + memberDTO.getAge());
+            // 페이징
+//            for(int i =0; i< 100; i++) {
+//                Member member = new Member();
+//                member.setUsername("member"+i);
+//                member.setAge(i);
+//                em.persist(member);
+//            }
+//
+//            em.flush();
+//            em.clear();
+//
+//            List<Member> result = em.createQuery("select m from Member m order by m.age desc", Member.class)
+//                    .setFirstResult(1)
+//                    .setMaxResults(10)
+//                    .getResultList();
+//
+//            System.out.println("result.size = " + result.size());
+//            for (Member member1 : result) {
+//                System.out.println("member1 = " + member1);
+//            }
+
+
 
             tx.commit();
         }catch(Exception e){
